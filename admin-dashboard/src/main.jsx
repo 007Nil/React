a1de1abfe35a938bf4f61b3ec1fce87e081a26ef
007Nil <sagniksarkar@ymail.com>
2023-05-07 02:06:51 +0530

    Added navbar and sidebar

--- admin-dashboard/src/main.jsx
+++ admin-dashboard/src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ContextProvider } from './contexts/ContextProvider';
+import { registerLicense } from '@syncfusion/ej2-base';
 
+// console.log(import.meta.env.VITE_syncfusion_license)
+registerLicense(import.meta.env.VITE_syncfusion_license);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+
   </React.StrictMode>,
 )
