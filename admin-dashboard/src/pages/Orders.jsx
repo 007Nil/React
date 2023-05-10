c7b50343c4790b3bf79323d8c902e70f6d6cd995
007Nil <sagniksarkar@ymail.com>
2023-05-11 02:29:04 +0530

    order imp in progress

--- admin-dashboard/src/pages/Orders.jsx
+++ admin-dashboard/src/pages/Orders.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {GridComponent,ColumnDirective, ColumnsDirective, Resize, Sort, ContextMenu, Filter } from '@syncfusion/ej2-react-grids'
 
 const Orders = () => {
   return (
