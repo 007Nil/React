089e3de2ed5b0bc038bcfcd5aa06e2e481a97a53
007Nil <sagniksarkar@ymail.com>
2023-05-14 03:22:40 +0530

    calender app done

--- admin-dashboard/src/pages/Calendar.jsx
+++ admin-dashboard/src/pages/Calendar.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-import {ScheduleComponent, ViewsDirective, ViewDirective, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop} from '@syncfusion/ej2-react-schedule'
+import { ScheduleComponent, ViewsDirective, ViewDirective, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop } from '@syncfusion/ej2-react-schedule'
 
-import {DatePickerComponent} from '@syncfusion/ej2-react-calendars'
+import { DatePickerComponent } from '@syncfusion/ej2-react-calendars'
 
 
 import { scheduleData } from '../data/dummy'
@@ -16,6 +16,16 @@ const Calendar = () => {
         catagory="App"
         title="Calender"
       />
+
+      <ScheduleComponent
+        height="650px"
+        eventSettings={{
+          dataSource: scheduleData
+        }}
+        selectedDate={new Date(2021,0,10)}
+      >
+        <Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]} />
+      </ScheduleComponent>
     </div>
   )
 }
