39fe84ccf4951e719dda03cbe0555ed1e7592b84
007Nil <sagniksarkar@ymail.com>
2023-05-14 18:51:36 +0530

    editor done

--- admin-dashboard/src/pages/Editor.jsx
+++ admin-dashboard/src/pages/Editor.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditor, Toolbar, RichTextEditorComponent } from '@syncfusion/ej2-react-richtexteditor';
+
+import { EditorData } from '../data/dummy';
+
+import { Header } from '../components';
+
 
 const Editor = () => {
   return (
-    <div>Editor</div>
+    <div className='m-2 md:m-10 mt-24 p-2 md:o-10 bg-white rounded-3xl'>
+      <Header
+        catagory="App"
+        title="Editor"
+      />
+
+      <RichTextEditorComponent>
+        <EditorData/>
+        <Inject services={[HtmlEditor, Toolbar
+          , Image, Link, QuickToolbar]} />
+      </RichTextEditorComponent>
+
+    </div>
   )
 }
 
