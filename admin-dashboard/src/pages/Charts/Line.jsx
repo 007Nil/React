0a92cb5f6a1aec36948b745629823ed29559ac8b
007Nil <sagniksarkar@ymail.com>
2023-05-22 02:53:29 +0530

    This project is done. Rest will do later

--- admin-dashboard/src/pages/Charts/Line.jsx
+++ admin-dashboard/src/pages/Charts/Line.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
+import { Header, LineChart } from '../../components'
+
+
+
 const Line = () => {
   return (
-    <div>Line</div>
+    <div className='m-4 md:m-20 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <Header
+        catagory='Chart'
+        title='Inflation Rate'
+      />
+
+      <div className='w-full'>
+        <LineChart/>
+      </div>
+
+    </div>
   )
 }
 
