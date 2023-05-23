672a994f28a51daf40946482c1618d4de7846e76
007Nil <sagniksarkar@ymail.com>
2023-05-24 01:28:32 +0530

    navbar done

--- 3d_portfolio/src/components/Navbar.jsx
+++ 3d_portfolio/src/components/Navbar.jsx
@@ -26,7 +26,9 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
 
-          <p className='text-white text-[18px] font-bold cursor-pointer'>Sagnik Sarkar <span className='sm:block hidden'>Cloud Engineer</span></p>
+          <p className='text-white text-[18px] font-bold cursor-pointer flex'>Sagnik&nbsp;
+            <span className='sm:block hidden'> | Cloud Engineer</span>
+          </p>
         </Link>
 
         <ul className='list-none hidden sm:flex flex-row gap-10'>
@@ -48,7 +50,7 @@ const Navbar = () => {
         </ul>
 
         <div className='sm:hidden flex flex-1 justify-end items-center'>
-          <img src={toggle ? menu : close}
+          <img src={toggle ? close : menu}
             alt="menu"
             className='w-[28px] h-[28px] object-contain cursor-pointer'
             onClick={() => {
@@ -57,11 +59,31 @@ const Navbar = () => {
           />
 
           <div className={
-            `${!toggle ? 'hidden' : 'flex'} p-6 absolute top-20`
+            `${!toggle ? 'hidden' : 'flex'} p-6 absolute top-20 right-0 mx-4 my-2 min-[140px] z-10 rounded-xl black-gradient`
           }>
 
-          </div>
+            <ul className='list-none flex justify-end items-start flex-col gap-4'>
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={
+                    `${active === link.title
+                      ? "text-white"
+                      : "text-secondary"
+                    } font-sans font-medium cursor-pointer text-[16px]`
+                  }
 
+                  onClick={() => {
+                    setToggle(!toggle)
+                    setActive(link.title)
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+
+          </div>
         </div>
 
 
