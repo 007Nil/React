2c72ab96145446783247096d8769fc0f819d2aa5
007Nil <sagniksarkar@ymail.com>
2023-05-25 02:20:11 +0530

    about done

--- 3d_portfolio/src/components/canvas/Computers.jsx
+++ 3d_portfolio/src/components/canvas/Computers.jsx
@@ -1,12 +1,12 @@
 import { Suspense, useEffect, useState } from 'react'
 
-import { Canvas } from '@react-three/fiber'
+import { Canvas, events } from '@react-three/fiber'
 
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 
 import CanvasLoader from '../Loader'
 
-const Computers = () => {
+const Computers = (isMobile) => {
 
   const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
@@ -15,7 +15,7 @@ const Computers = () => {
 
       <pointLight intensity={1} />
       <spotLight
-        position={[-20,15,10]}
+        position={[-20, 15, 10]}
         angle={0.12}
         penumbra={1}
         intensity={1}
@@ -24,16 +24,39 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
-        rotation={[-0.01,-0.2,-0.1]}
-        />
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
 
     </mesh>
   )
 }
 
 const ComputerCanvas = () => {
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+
+    }
+
+  }, []);
+
+
   return (
     <Canvas
       frameloop='demand'
@@ -48,7 +71,7 @@ const ComputerCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
