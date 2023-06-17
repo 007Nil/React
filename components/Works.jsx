ef3f45df4a6586b4bf8787a9c7e1d10ef1df3df5
007Nil <sagniksarkar@ymail.com>
2023-06-18 00:06:18 +0530

    worked on the Works section

--- components/Works.jsx
+++ components/Works.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import Tilt from 'react-parallax-tilt'
+import { motion } from 'framer-motion'
 
+import {styles} from '..'
 const Works = () => {
   return (
     <div>Works</div>
